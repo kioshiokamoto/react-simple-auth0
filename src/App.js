830ee71b770d020959bc13d37c2bfdada2d1e6b9
Kioshi Okamoto <kjor29@gmail.com>
2021-03-17 22:21:59 -0500

    :fire: IMPROVE: Modificacion de componente para visualizacion de login/logout

--- src/App.js
+++ src/App.js
@@ -2,20 +2,20 @@ import './App.css';
 import LoginButton from './components/LoginButton';
 import LogoutButton from './components/LogoutButton';
 import Profile from './components/Profile';
+import { useAuth0 } from '@auth0/auth0-react';
 
 function App() {
+	const { user, isAuthenticated, isLoading } = useAuth0();
 	return (
-		<>
+		<div>
 			<h1>Test Auth0</h1>
 			<hr />
 			<div>
 				<Profile />
 			</div>
-			<div>
-				<LoginButton />
-				<LogoutButton />
-			</div>
-		</>
+
+			{!isAuthenticated ? <LoginButton /> : <LogoutButton />}
+		</div>
 	);
 }
 
